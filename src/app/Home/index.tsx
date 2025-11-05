8bc3cfdd19ab84c02ffb456a4758dbbc46637819
AmandaCMLFerraz <amandaferraz@unochapeco.edu.br>
2025-11-05 13:43:35 -0300

    feat: Created filter components and added them to the home screen

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,8 +1,15 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity, Text } from "react-native";
 
 import { styles } from "./styles"
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [
+  FilterStatus.PENDING,
+  FilterStatus.DONE
+]
 
 export function Home() {
   return (
@@ -17,7 +24,17 @@ export function Home() {
       </View>
 
       <View style={styles.content}>
+        <View style={styles.header}>
+          {
+            FILTER_STATUS.map((status) => (
+              <Filter key={status} status={status} isActive />
 
+            ))
+          }
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   )
