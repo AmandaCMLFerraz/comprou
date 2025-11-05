8aab3815dbad5bc6d165a1e8ee7450abe869e885
AmandaCMLFerraz <amandaferraz@unochapeco.edu.br>
2025-11-05 13:02:59 -0300

    feat: Tela inicial ajustada com os novos componentes

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,11 +1,24 @@
 import { View, Image } from "react-native";
 
 import { styles } from "./styles"
+import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")}/>
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?"/>
+        <Button 
+          title="Adicionar"
+        />
+      </View>
+
+      <View style={styles.content}>
+
+      </View>
     </View>
   )
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -4,7 +4,23 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#D0D2D8",
+    paddingTop: 62,
   },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
+  }
 })
